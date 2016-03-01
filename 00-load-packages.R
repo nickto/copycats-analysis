04702cf11a6c6573ae8aae0542b6b4a44639d0d6
nickto <weederdam@gmail.com>
2016-03-01 23:31:29 +0200

    Add: load plyr

--- 00-load-packages.R
+++ 00-load-packages.R
@@ -1,6 +1,8 @@
 if (!require("ff")) install.packages("ff")
 if (!require("ffbase")) install.packages("ffbase")
+if (!require("plyr")) install.packages("plyr")
 # If the above command does not work, then install from another repo:
 # install.packages('ff', repo='http://nbcgib.uesc.br/mirrors/cran/')
 library(ff)
 library(ffbase)
+library(plyr)
