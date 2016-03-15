7a5f3e02f80d1b2a890db457dc6438828f652206
nickto <weederdam@gmail.com>
2016-03-15 16:00:09 +0200

    Do not load ff* packages

--- 00-load-packages.R
+++ 00-load-packages.R
@@ -1,13 +1,13 @@
-if (!require("ff")) install.packages("ff")
-if (!require("ffbase")) install.packages("ffbase")
+#if (!require("ff")) install.packages("ff")
+#if (!require("ffbase")) install.packages("ffbase")
 if (!require("plyr")) install.packages("plyr")
 if (!require("RPostgreSQL")) install.packages("RPostgreSQL")
 if (!require("ggplot2")) install.packages("ggplot2")
 
 # If the above command does not work, then install from another repo:
 # install.packages('ff', repo='http://nbcgib.uesc.br/mirrors/cran/')
-library(ff)
-library(ffbase)
+#library(ff)
+#library(ffbase)
 library(plyr)
 library(RPostgreSQL)
 library(ggplot2)
