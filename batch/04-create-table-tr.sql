549f3f894c7620aae078ddfe560c13a79718614c
nickto <weederdam@gmail.com>
2016-03-15 14:09:41 +0200

    Remove primary keys

--- batch/04-create-table-tr.sql
+++ batch/04-create-table-tr.sql
@@ -8,6 +8,37 @@ tr.fund_characteristics (
     assets integer,
     ioc smallint,
     prdate date,
-    country varchar(32),
-    PRIMARY KEY(fdate, fundno)
-)
+    country varchar(32)
+);
+
+CREATE TABLE
+tr.stock_characteristics (
+    fdate date,
+	cusip varchar(8),
+	stkname varchar(32),
+	ticker varchar(4),
+	ticker2 varchar(6), 
+	exchcd varchar(16),
+	stkcd varchar(16),
+	stkcdesc varchar(16),
+	shrout1 smallint,
+	prc numeric(10, 4),
+	shrout2 integer,
+	indcode smallint
+);
+
+CREATE TABLE
+tr.holdings (
+    fdate date,
+	cusip varchar(8),
+    fundno integer,
+	shares bigint
+);
+
+CREATE TABLE
+tr.holdings_change (
+    fdate date,
+	cusip varchar(8),
+    fundno integer,
+	change bigint
+);
