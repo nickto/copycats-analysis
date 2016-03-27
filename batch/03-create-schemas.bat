c9ff27dd6aa49947fbc64bd4a566c9f1141a34bb
nickto <weederdam@gmail.com>
2016-03-27 17:20:20 +0300

    Create crsp schema

--- batch/03-create-schemas.bat
+++ batch/03-create-schemas.bat
@@ -3,5 +3,6 @@ SET username=%1
 SET postgreBin=%2
 
 %postgreBin%psql.exe -a -f "%~dp0\03-create-schema-tr.sql" copycats %username%
+%postgreBin%psql.exe -a -f "%~dp0\03-create-schema-crsp.sql" copycats %username%
 
-ECHO Schema "tr" for Thomson Reuters created succesfully.
+ECHO If not error messages were shown, schemas "tr" and "crsp" were created succesfully.
