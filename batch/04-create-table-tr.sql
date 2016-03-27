471703d32aca999ee8ed8130a25dd0618146fa0d
nickto <weederdam@gmail.com>
2016-03-27 17:44:59 +0300

    Change types to upper case

--- batch/04-create-table-tr.sql
+++ batch/04-create-table-tr.sql
@@ -1,44 +1,44 @@
 CREATE TABLE
 tr.fund_characteristics (
-    fdate date,
-    fundno integer,
-    fundname varchar(32),
-    mgrcoab varchar(8),
-    rdate date,
-    assets integer,
-    ioc smallint,
-    prdate date,
-    country varchar(32)
+    fdate DATE,
+    fundno INTEGER,
+    fundname VARCHAR(32),
+    mgrcoab VARCHAR(8),
+    rdate DATE,
+    assets INTEGER,
+    ioc SMALLINT,
+    prdate DATE,
+    country VARCHAR(32)
 );
 
 CREATE TABLE
 tr.stock_characteristics (
-    fdate date,
-	cusip varchar(8),
-	stkname varchar(32),
-	ticker varchar(4),
-	ticker2 varchar(6), 
-	exchcd varchar(16),
-	stkcd varchar(16),
-	stkcdesc varchar(16),
-	shrout1 smallint,
-	prc numeric(10, 4),
-	shrout2 integer,
-	indcode smallint
+    fdate DATE,
+	cusip VARCHAR(8),
+	stkname VARCHAR(32),
+	ticker VARCHAR(4),
+	ticker2 VARCHAR(6), 
+	exchcd VARCHAR(16),
+	stkcd VARCHAR(16),
+	stkcdesc VARCHAR(16),
+	shrout1 SMALLINT,
+	prc NUMERIC(10, 4),
+	shrout2 INTEGER,
+	indcode SMALLINT
 );
 
 CREATE TABLE
 tr.holdings (
-    fdate date,
-	cusip varchar(8),
-    fundno integer,
-	shares bigint
+    fdate DATE,
+	cusip VARCHAR(8),
+    fundno INTEGER,
+	shares BIGINT
 );
 
 CREATE TABLE
 tr.holdings_change (
-    fdate date,
-	cusip varchar(8),
-    fundno integer,
-	change bigint
+    fdate DATE,
+	cusip VARCHAR(8),
+    fundno INTEGER,
+	change BIGINT
 );
