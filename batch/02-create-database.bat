322f955e89265d13d3372bc70249cc486c81f138
nickto <weederdam@gmail.com>
2016-04-07 20:18:00 +0300

    Move batch files to archive

--- batch/02-create-database.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-SET username=%1
-SET postgreBin=%2
-
-%postgreBin%psql.exe -a -f "%~dp0\02-create-db-copycats.sql" postgres %username%
-
-ECHO If no error messages were shown, database "copycats" created succesfully.
--- batch/04-create-tables.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET username=%1
-SET postgreBin=%2
-
-::%postgreBin%psql.exe -a -f "%~dp0\04-create-table-tr.sql" copycats %username%
-%postgreBin%psql.exe -a -f "%~dp0\04-create-table-crsp.sql" copycats %username%
-
-ECHO Tables created succesfully.
-
